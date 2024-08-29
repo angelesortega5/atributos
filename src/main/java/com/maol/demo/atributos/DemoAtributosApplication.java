3ecef84c6b3be161226b7ee67aa6d0905dfd5d21
angelesortega <mariaortega5l@outlook.com>
2024-08-29 18:19:07 +0200

    atributos

---
+++ src/main/java/com/maol/demo/atributos/DemoAtributosApplication.java
@@ -0,0 +1,13 @@
+package com.maol.demo.atributos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAtributosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAtributosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maol/demo/atributos/DemoAtributosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maol.demo.atributos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAtributosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
